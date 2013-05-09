ee890ea3d4e5b6ce94f537bec1de2c2ceb058262
Stephen Doody <sd@xrulz.com>
2013-05-09 14:57:24 -0400

    Updates to command files to preserve separable versions of lipstick across siebel-web-determinations and web-determinations

--- ky_ui_fc/xrulzBuildAndDeploy2AllTomcat7.bat
+++ ky_ui_fc/xrulzBuildAndDeploy2AllTomcat7.bat
@@ -5,7 +5,8 @@ rem --------------------------------
 rem Paths
 rem --------------------------------
 set sourcePath=C:\Dev\RB\KY_Rulebase\ky_ui_fc\Development\output
-set targetPath=C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\web-determinations\WEB-INF\classes\rulebases
+set targetPath1=C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\rulebases
+set targetPath2=C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\rulebases
 
 set opm_compiler=C:\Program Files (x86)\Oracle\Policy Modeling\bin\Oracle.Policy.Modeling.CommandLineCompiler.exe
 set rb_ky_fpr=C:\Dev\RB\KY_Rulebase\ky_fpr\Development\ky_fpr.xprj
@@ -21,14 +22,17 @@ echo Building OWD: %rb_ky_ui_fc%
 IF ERRORLEVEL 1 GOTO fail02
 
 rem --------------------------------- 
-set configPath="%targetPath%"
+set configPath1="%targetPath1%"
+set configPath2="%targetPath2%"
 rem set imagesPath="%targetPath%"\images\
 rem set resourcePath="%targetPath%"\resources\
 rem set templatePath="%targetPath%"\templates\
 rem --------
 
-robocopy %sourcePath% %configPath% *.rmod *.zip /TS /NP /LOG+:build_output.log
+robocopy %sourcePath% %configPath1% *.rmod *.zip /TS /NP /LOG+:build_output.log
 IF ERRORLEVEL 4 GOTO fail03
+robocopy %sourcePath% %configPath2% *.rmod *.zip /TS /NP /LOG+:build_output.log
+IF ERRORLEVEL 4 GOTO fail04
 
 echo OPM Build Success... Output Rulebases copied to Tomcat 7
 EXIT /B 0
@@ -40,7 +44,9 @@ EXIT /B %ERRORLEVEL%
 echo Fail 02, Your OWD Pixels are not shiny enough, ended badly with code %ERRORLEVEL%
 EXIT /B %ERRORLEVEL%
 :fail03 
-echo Fail 03, Robust Copy to Tomcat 7, ended badly with code %ERRORLEVEL%
+echo Fail 03, Robust Copy to Tomcat 7, siebel-web-determinations, ended badly with code %ERRORLEVEL%
 EXIT /B %ERRORLEVEL%
-
+:fail04 
+echo Fail 04, Robust Copy to Tomcat 7, web-determinations, ended badly with code %ERRORLEVEL%
+EXIT /B %ERRORLEVEL%
 
