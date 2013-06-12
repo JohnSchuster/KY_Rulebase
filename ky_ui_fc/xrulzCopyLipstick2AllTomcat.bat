793a3e4f9a17ed93ea6fdde6cbb60ce08017e3f9
Stephen Doody <sd@xrulz.com>
2013-06-12 14:49:08 -0400

    fixups to re-open and also build file

--- ky_ui_fc/xrulzCopyLipstick2AllTomcat.bat
+++ ky_ui_fc/xrulzCopyLipstick2AllTomcat.bat
@@ -1,9 +1,10 @@
 echo off
 set local
 rem --------
-rem SD: 2013-05-29
+rem SD: 2013-06-12
 rem --------
 echo ---
+echo Changes made to how this cross-copies lipstick to web-determinations
 echo Copies Lipstick, to Tomcat 7.0 siebel-web-determinations, web-determinations, and OPM Release
 echo Note: Synchronizes siebel-web-determinations (baseline) to web-determinations (sync target)
 echo ---
@@ -14,20 +15,22 @@ goto :abort
 :doit
 echo off
 
-set sourcePath0=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\siebel-web-determinations\WEB-INF\classes\templates
+set sourcePath0=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\siebel-web-determinations\WEB-INF\classes
 set sourcePath1=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\siebel-web-determinations\WEB-INF
 set sourcePath2=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\web-determinations\WEB-INF
 
-set targetPath_00=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\web-determinations\WEB-INF\classes\templates
+set targetPath_00=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\web-determinations\WEB-INF\classes
 set targetPath_01="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF"
 set targetPath_02="C:\Dev\RB\KY_Rulebase\ky_ui_fc\Release\web-determinations\WEB-INF"
 set targetPath_03="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\web-determinations\WEB-INF"
 
 set exclude_01="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\rulebases"
+set exclude_02="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\plugins"
+
 rem --------
 echo on
 echo Synchronizing siebel-web-determinations to web-determinations
-robocopy %sourcePath0% %targetPath_00% /S /IS /NP /XD /LOG+:build_output.log
+robocopy %sourcePath0% %targetPath_00% /S /IS /NP /XD %exclude_02% /LOG+:build_output.log
 IF ERRORLEVEL 4 GOTO fail04
 robocopy %sourcePath1% %targetPath_01% /S /IS /NP /XD %exclude_01% /LOG+:build_output.log
 IF ERRORLEVEL 4 GOTO fail04
