ddcc4cd5077022f3c0e05deb81b5599b4fa7dc11
Stephen Doody <sd@xrulz.com>
2013-07-05 10:26:36 -0400

    Build and xRulz Handler Changes

--- ky_ui_fc/xrulzCopyLipstick2AllTomcat.bat
+++ ky_ui_fc/xrulzCopyLipstick2AllTomcat.bat
@@ -1,7 +1,7 @@
 echo off
 set local
 rem --------
-rem SD: 2013-06-12
+rem SD: 2013-07-05
 rem --------
 echo ---
 echo Changes made to how this cross-copies lipstick to web-determinations
@@ -18,18 +18,19 @@ echo off
 set sourcePath0=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\siebel-web-determinations\WEB-INF\classes
 set sourcePath1=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\siebel-web-determinations\WEB-INF
 set sourcePath2=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\web-determinations\WEB-INF
+set sourcePath3=C:\Dev\RB\KY_Rulebase\Tomcat\lib
 
 set targetPath_00=C:\Dev\RB\KY_Rulebase\Tomcat\webapps\web-determinations\WEB-INF\classes
 set targetPath_01="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF"
 set targetPath_02="C:\Dev\RB\KY_Rulebase\ky_ui_fc\Release\web-determinations\WEB-INF"
 set targetPath_03="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\web-determinations\WEB-INF"
+set targetPath_04="C:\Program Files\Apache Software Foundation\Tomcat 7.0\lib"
 
 set exclude_01="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\rulebases"
 set exclude_02="C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\siebel-web-determinations\WEB-INF\classes\plugins"
 
 rem --------
-echo on
-echo Synchronizing siebel-web-determinations to web-determinations
+echo Synchronizing From Git To Local Tomcat Deployment
 robocopy %sourcePath0% %targetPath_00% /S /IS /NP /XD %exclude_02% /LOG+:build_output.log
 IF ERRORLEVEL 4 GOTO fail04
 robocopy %sourcePath1% %targetPath_01% /S /IS /NP /XD %exclude_01% /LOG+:build_output.log
@@ -38,7 +39,9 @@ robocopy %sourcePath2% %targetPath_02% /S /IS /NP /XD %exclude_01% /LOG+:build_o
 IF ERRORLEVEL 4 GOTO fail04
 robocopy %sourcePath2% %targetPath_03% /S /IS /NP /XD %exclude_01% /LOG+:build_output.log
 IF ERRORLEVEL 4 GOTO fail04
-echo All Copies Completed.  Restart Tomcat 7 and Test!
+robocopy %sourcePath3% %targetPath_04% /S /IS /NP /XD /LOG+:build_output.log
+IF ERRORLEVEL 4 GOTO fail04
+echo All Copy steps completed.
 exit /B 0
 
 :abort
